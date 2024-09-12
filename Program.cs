0ea917509c16806cf76afc6a54fd2450be69d9df
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-12 20:10:20 +0300

    Sum all items of list

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rows:");
+            int rows = int.Parse(Console.ReadLine());
+            List<int> currentRow = new List<int>();
+            double sum = 0;
+
+            Console.WriteLine("Enter list elements:");
+            for (int i = 0; i < rows; i++)
+            {
+                currentRow = Console.ReadLine().Split().Select(int.Parse).ToList();
+                foreach (int item in currentRow)
+                {
+                    sum += item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
